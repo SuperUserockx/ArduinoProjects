32b6d86d26def908441ffe677c39eb5f94fb3935
Pinaq Sharma <pinaq1234@gmail.com>
2020-09-10 15:53:53 +0530

    Add files via upload

---
+++ Displays/oled_serial/oled_serial.ino
@@ -0,0 +1,192 @@
+
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+SoftwareSerial mySerial(10, 11);
+TinyGPS gps;  
+
+void gpsdump(TinyGPS &gps);
+void printFloat(double f, int digits = 2);
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()   {
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 5v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done
+  // Show image buffer on the display hardware.
+
+  display.display();
+  //delay(2000);
+
+
+  // invert the display
+  display.invertDisplay(true);
+  //delay(1000);
+  display.invertDisplay(false);
+  //delay(1000);
+  display.display();
+  //delay(2000);
+   display.clearDisplay();
+
+
+  mySerial.begin(9600);
+  /*delay(1000);
+  Serial.println("uBlox Neo 6M");
+  Serial.print("Testing TinyGPS library v. "); Serial.println(TinyGPS::library_version());
+  Serial.println("by Mikal Hart");
+  Serial.println();
+  Serial.print("Sizeof(gpsobject) = "); 
+  Serial.println(sizeof(TinyGPS));
+  Serial.println(); 
+
+  */
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10, 0);
+  display.clearDisplay();
+  display.println("CONNECTIONG");
+}
+void loop() // run over and over
+{
+  bool newdata = false;
+  unsigned long start = millis();
+  // Every 5 seconds we print an update
+  while (millis() - start < 5000) 
+  {
+    if (mySerial.available()) 
+    
+    {
+      char c = mySerial.read();
+      //Serial.print(c);  // uncomment to see raw GPS data
+      if (gps.encode(c)) 
+      {
+        newdata = true;
+        break;  // uncomment to print new data immediately!
+      }
+    }
+  }
+  
+  if (newdata) 
+  {
+    
+    gpsdump(gps);
+    
+  }
+  
+}
+
+void gpsdump(TinyGPS &gps)
+{
+  
+  
+display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 0);
+    display.clearDisplay();
+    display.println("SPEED : ");
+    display.println(gps.f_speed_kmph());
+    display.display();
+    display.println("\n");
+  
+
+
+
+  
+  
+}
+
+void printFloat(double number, int digits)
+{
+  // Handle negative numbers
+  if (number < 0.0) 
+  {
+     Serial.print('-');
+     number = -number;
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+  
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+ 
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0)
+    Serial.print("."); 
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0) 
+  {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    Serial.print(toPrint);
+    remainder -= toPrint;
+  }
+}
+/*
+void loop() {
+  while (1) {
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(10, 0);
+    display.clearDisplay();
+    display.println("input:");
+    String s;
+  if (Serial.available())
+  {
+    s = Serial.readString();  //getting string input in variable "s"
+    display.println(s);
+    display.display();
+    display.println("\n");
+  }
+   // display.display();
+    
+  }
+
+}
+*/
